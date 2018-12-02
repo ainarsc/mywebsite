15d1ab3e4fd9b9f81f2e84cdbb15a664af9ca736
Ainars Ciesa <ainarsciesa@CeXs-MacBook-Pro-2.local>
2018-12-02 19:47:05 +0700

    Updates

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// Selection of DOM items
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-items');
+
+// Set initial state of the menu
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+    showMenu = true; //change the menu state to ture as its being shown;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+    showMenu = false;
+  }
+
+}
+
+
+function showMore(postNr, dotNr, btnNr) {
+  var dots = document.getElementById(dotNr);
+  var moreText = document.getElementById(postNr);
+  var btnText = document.getElementById(btnNr);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
